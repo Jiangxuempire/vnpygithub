0a89ce1be8523d72201cc0f3701c19a70e30eb07
yunya <yxl560@qq.com>
2020-07-02 21:05:27 +0800

    jupyter notebook 主题设置

---
+++ jupyter_notebook主题设置.py
@@ -0,0 +1,22 @@
+# _*_coding : UTF-8 _*_
+#开发团队 ：yunya
+#开发人员 ：yunya
+#开发时间 : 2020/7/2 21:01
+#文件名称 ：jupyter_notebook主题设置.py
+#开发工具 ： PyCharm
+
+
+# https://www.cnblogs.com/shanger/p/12006161.html
+# 1、安装 Jupyter 主题
+#
+# pip install jupyterthemes
+# 2、更新 Jupyter 主题 （可选）
+#
+# pip install --upgrade jupyterthemes
+
+# 3、查看可用的 Jupyter 主题
+#
+# jt -l
+
+
+#   jt -t onedork -f fira -fs 13 -cellw 80% -ofs 12 -dfs 12 -T -T

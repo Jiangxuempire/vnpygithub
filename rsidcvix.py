8f99e1a5fce7afa2187f5c2b3e215f6ad33768f9
yunya <yxl560@aliyun.com>
2020-09-24 14:48:22 +0800

    ris 上下轨 使用 dc 方式取得（np容器方式）增加 均线移动止损

--- rsidcvix.py
+++ rsidcvix.py
@@ -32,17 +32,20 @@ class RsiDcVixStrategy(CtaTemplate):
 
     author = "yunya"
 
-    open_window = 15
-    rsi_window = 50
-    exit_window = 50
+    open_window = 45
+    rsi_window = 100
+    exit_window = 65
+    ema_window = 65
     atr_window = 16
+    atr_window_ma = 10
     atr_multiplier = 10.0
-    pos_trailing = 5.0
+    pos_trailing = 3.0
     fixed_size = 1
 
     exit_up = 0
     exit_down = 0
     atr_value = 0
+    atr_value_ma = 0
     long_entry = 0
     short_entry = 0
     long_stop = 0
@@ -57,11 +60,21 @@ class RsiDcVixStrategy(CtaTemplate):
     rsi_up_last = 0
     rsi_down_current = 0
     rsi_down_last = 0
+    exit_long_nex = 0
+    exit_long_last = 0
+    exit_long = 0
+    exit_short_nex = 0
+    exit_short_last = 0
+    exit_short = 0
+    boll_mid = 0
+    boll_mid_new = 0
+    ema_window_new = 0
 
     parameters = [
             "open_window",
             "rsi_window",
             "exit_window",
+            "ema_window",
             "atr_window",
             "atr_multiplier",
             "pos_trailing",
@@ -146,9 +159,10 @@ class RsiDcVixStrategy(CtaTemplate):
         # 计算 dev_max
         rsi_array = talib.RSI(self.am.close, self.rsi_window)
 
-        if not self.rsi_inited and self.count <= self.size:
+        if not self.rsi_inited:
             self.count += 1
-            self.rsi_inited = True
+            if self.count >= self.size:
+                self.rsi_inited = True
 
         # rsi 上下轨容器
         self.rsi_up_array[:-1] = self.rsi_up_array[1:]
@@ -175,21 +189,31 @@ class RsiDcVixStrategy(CtaTemplate):
         self.rsi_down_last = self.rsi_down_array[-3]
 
         self.exit_up, self.exit_down = self.am.donchian(self.exit_window)
-        self.atr_value = am.atr(self.atr_window)
+        atr_value_array = am.atr(self.atr_window, True)
+        self.atr_value = atr_value_array[-1]
+        self.atr_value_ma = atr_value_array[-self.atr_window_ma:].mean()
+
+        self.boll_mid = am.sma(self.ema_window)
 
         # 没有仓
         if not self.pos:
             self.intra_trade_high = bar.high_price
             self.intra_trade_low = bar.low_price
+            rsi_inited_up = self.rsi_value_current > self.rsi_up_current and self.rsi_value_last <= self.rsi_up_last
+            rsi_inited_down = self.rsi_value_current < self.rsi_down_current and self.rsi_value_last >= self.rsi_down_last
+            atr_inited = self.atr_value > self.atr_window_ma
 
-            if self.rsi_value_current > self.rsi_up_current and self.rsi_value_last <= self.rsi_up_last:
+            self.ema_window_new = self.ema_window
+
+            if rsi_inited_up and atr_inited and self.rsi_value_current > 50:
                 self.buy(bar.close_price + 5, self.fixed_size)
 
-            elif self.rsi_value_current < self.rsi_down_current and self.rsi_value_last >= self.rsi_down_last:
+            elif rsi_inited_down and atr_inited and self.rsi_value_current < 50:
                 self.short(bar.close_price - 5, self.fixed_size)
 
         # 有多仓
         elif self.pos > 0:
+
             self.intra_trade_high = max(self.intra_trade_high, bar.high_price)
             self.intra_trade_low = bar.low_price
             self.long_stop = self.intra_trade_high - self.atr_value * self.atr_multiplier
@@ -197,16 +221,86 @@ class RsiDcVixStrategy(CtaTemplate):
             self.long_stop = max(self.long_stop, self.exit_down)
             self.long_stop = max(self.long_stop, self.long_trade_stop)
 
+            # 上涨或下跌时，布林中轨均值减1
+            close_long = am.close[-1] > am.close[-2] > am.close[-3]
+
+            if close_long:
+                self.ema_window_new -= 1
+                self.ema_window_new = max(self.ema_window_new, 10)
+
+            # 计算新的布林带
+            self.boll_mid_new = am.sma(self.ema_window_new, True)
+
+            # 仓位是long 时，如果价格上穿新布林中轨
+            con1 = am.close[-1] < self.boll_mid_new[-1]
+            con2 = am.close[-2] >= self.boll_mid_new[-2]
+
+            if con1 and con2:
+                self.exit_long_nex = am.close[-1]  # 保存当前收盘价
+                if self.exit_long_nex > self.exit_long_last or self.exit_long_last == 0:
+                    self.exit_long_last = self.exit_long_nex
+                    self.ema_window_new = self.ema_window
+                    # 下穿新均线，以原布林均线挂出停止单，避免快速下跌而无法止损
+                    self.exit_long = self.boll_mid
+                    # print(f"我是多单，exitlast:{self.exit_short_last},重置布林中轨参数，止损挂{self.exit_long}：")
+                else:
+                    # 收盘价在两条均线平均价上方，以当前收盘价挂出限价单
+                    if self.am.close[-1] > ((self.boll_mid + self.boll_mid_new[-1]) / 2):
+                        self.exit_long = bar.close_price
+                        # print(f"我是多单，收盘价在两个中轨均值上方，以收盘价挂止损单:{self.exit_long}")
+                    elif bar.close_price < self.boll_mid:
+                        self.exit_long = bar.close_price
+                    else:
+                        self.exit_long = self.boll_mid
+                        # print(f"我是多单，收盘价在两个中轨均值下方，以原中轨挂止损单:{self.exit_long},")
+            else:
+                self.exit_long = self.boll_mid
+
+            self.long_stop = max(self.long_stop, self.exit_long)
+
             self.sell(self.long_stop, abs(self.pos), True)
 
         # 有空仓
         elif self.pos < 0:
+
             self.intra_trade_high = bar.high_price
             self.intra_trade_low = min(self.intra_trade_low, bar.low_price)
             self.short_stop = self.intra_trade_low + self.atr_value * self.atr_multiplier
 
             self.short_stop = min(self.short_stop, self.exit_up)
             self.short_stop = min(self.short_stop, self.short_trade_stop)
+
+            close_short = am.close[-1] < am.close[-2] < am.close[-3]
+            if close_short:
+                self.ema_window_new -= 1
+                self.ema_window_new = max(self.ema_window_new, 10)
+
+            # 计算新的布林带
+            self.boll_mid_new = am.sma(self.ema_window_new, True)
+
+            # 仓位是short 时，如果价格上穿新布林中轨
+            con3 = am.close[-1] > self.boll_mid_new[-1]
+            con4 = am.close[-2] <= self.boll_mid_new[-2]
+
+            if con3 and con4:
+                self.exit_short_nex = am.close[-1]
+                if self.exit_short_nex < self.exit_short_last or self.exit_short_last == 0:
+                    self.exit_short_last = self.exit_short_nex
+                    self.ema_window_new = self.ema_window
+
+                    self.exit_short = self.boll_mid
+                else:
+                    if self.am.close[-1] < (self.boll_mid + self.boll_mid_new[-1] / 2):
+                        self.exit_short = bar.close_price
+
+                    elif bar.close_price > self.boll_mid:
+                        self.exit_short = bar.close_price
+                    else:
+                        self.exit_short = self.boll_mid
+            else:
+                self.exit_short = self.boll_mid
+
+            self.short_stop = min(self.short_stop, self.exit_short)
 
             self.cover(self.short_stop, abs(self.pos), True)
 
